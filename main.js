bbcda90baed6777768926ae4edc07061517ce4e8
Ritesh Saxena <riteshsaxena1998@gmail.com>
2020-12-27 15:44:55 +0530

    A mini project on javascript. In this project  I used some of the basic concepts of js and html dom.
    A mini project on javascript.
    In this project  I used some of the basic concepts of js and html dom.

---
+++ main.js
@@ -0,0 +1,21 @@
+function playSound(e) {
+    console.log(e);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    console.log(audio);
+    console.log(key);
+    if(!audio){
+        return;
+    }
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+
+}
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
